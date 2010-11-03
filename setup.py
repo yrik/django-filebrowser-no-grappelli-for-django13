42435123c5f8e798165a58ed5e98d8c2f844cd82
nicolas <nicolas@fierrito.localdomain>
2010-11-03 01:38:07 -0300

    added media, templates and locale to setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,11 @@ setup(
     url='http://code.google.com/p/django-filebrowser/',
     packages=find_packages(),
     include_package_data=True,
+    package_data = {'filebrowser': ['templates/filebrowser/*.html',
+                                    'templates/filebrowser/include/*',
+                                    'locale/*/LC_MESSAGES/*',
+                                    'media/filebrowser/*/*'],
+                    },
     zip_safe=False,
     classifiers=[
         'Development Status :: 4 - Beta',
