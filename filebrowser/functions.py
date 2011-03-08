9f80786efb7f65adeddb2b125eecdbed21086d0a
msaelices <msaelices@yaco.es>
2011-03-09 00:32:24 +0100

    More robust uploading in Django 1.3 when the FILEBROWSER_MEDIA_ROOT is outside Django MEDIA_ROOT (i.e. if is in STATIC_ROOT).

--- filebrowser/functions.py
+++ filebrowser/functions.py
@@ -9,7 +9,7 @@ from urlparse import urlparse
 from django.utils.translation import ugettext as _
 from django.utils.safestring import mark_safe
 from django.core.files import File
-from django.core.files.storage import default_storage
+from django.core.files.storage import FileSystemStorage
 from django.utils.encoding import smart_str
 
 # filebrowser imports
@@ -237,7 +237,9 @@ def handle_file_upload(path, file):
     """
     
     file_path = os.path.join(path, file.name)
-    uploadedfile = default_storage.save(file_path, file)
+    import ipdb; ipdb.set_trace()
+    storage = FileSystemStorage(location=MEDIA_ROOT)
+    uploadedfile = storage.save(file_path, file)
     return uploadedfile
 
 
